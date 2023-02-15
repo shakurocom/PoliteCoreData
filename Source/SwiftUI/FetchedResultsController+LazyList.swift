dc4413d0d920149b6dc698d5bd3a907aac4d3923
sshabalin <sshabalin@shakuro.com>
2023-02-15 16:27:19 +0300

    added LazyList to PoliteCoreData Framework

---
+++ Source/SwiftUI/FetchedResultsController+LazyList.swift
@@ -0,0 +1,61 @@
+//
+//
+//
+
+import Foundation
+
+extension FetchedResultsController {
+
+    public struct Section: Identifiable {
+
+        public let id: UUID = UUID()
+
+        public let title: String?
+
+        public let items: LazyList<ResultType>
+
+        public init?(title: String?, items: LazyList<ResultType>?) {
+            guard let items = items else {
+                return nil
+            }
+
+            self.title = title
+            self.items = items
+        }
+
+    }
+
+    public var hasFetchedObjects: Bool {
+        return fetchedResultsController.fetchedObjects != nil
+    }
+
+    public func lazySections() -> LazyList<Section> {
+        return LazyList(capacity: numberOfSections()) { [weak self] section in
+            return Section(title: self?.sectionName(section),
+                           items: self?.lazyItems(in: section))
+        }
+    }
+
+    public func lazyItems(in section: Int = 0) -> LazyList<ResultType> {
+        return LazyList(capacity: numberOfItemsInSection(section)) { [weak self] item in
+            return self?.item(indexPath: IndexPath(item: item, section: section))
+        }
+    }
+
+}
+
+extension FetchedResultsController.Section: Equatable where ResultType: Equatable {
+
+    public static func == (lhs: FetchedResultsController<EntityType, ResultType>.Section, rhs: FetchedResultsController<EntityType, ResultType>.Section) -> Bool {
+        return lhs.title == rhs.title && lhs.items == rhs.items
+    }
+
+}
+
+extension FetchedResultsController.Section: Hashable where ResultType: Equatable {
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+}
