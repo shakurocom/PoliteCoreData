1a820989de6db575aac534e5011cc2e99ace75b9
sshabalin <sshabalin@shakuro.com>
2023-09-27 19:24:30 +0300

    supporting WrapResult Indetifiable, Equtable, Hashable

--- Source/SwiftUI/FetchedResultsController+WrappedResult.swift
+++ Source/SwiftUI/FetchedResultsController+WrappedResult.swift
@@ -18,27 +18,39 @@ extension FetchedResultsController {
 
 extension FetchedResultsController.WrappedResult: Identifiable {
 
-    public var id: Int {
-        return hashValue
+    public var id: String {
+        switch self {
+        case .value(let element):
+            return element.objectID.uriRepresentation().absoluteString
+        case .empty(let section, let row):
+            return "section_\(section)-row_\(row)"
+        }
     }
 
 }
 
-extension FetchedResultsController.WrappedResult: Equatable {
+extension FetchedResultsController.WrappedResult: Equatable where ResultType: Equatable {
 
     public static func == (lhs: FetchedResultsController<EntityType, ResultType>.WrappedResult,
                            rhs: FetchedResultsController<EntityType, ResultType>.WrappedResult) -> Bool {
-        return lhs.id == rhs.id
+        switch (lhs, rhs) {
+        case (let .value(lhsElement), let .value(rhsElement)):
+            return lhsElement == rhsElement
+        case (let .empty(lhsSection, lhsRow), let .empty(rhsSection, rhsRow)):
+            return lhsSection == rhsSection && lhsRow == rhsRow
+        default:
+            return false
+        }
     }
 
 }
 
-extension FetchedResultsController.WrappedResult: Hashable {
+extension FetchedResultsController.WrappedResult: Hashable where ResultType: Hashable {
 
     public func hash(into hasher: inout Hasher) {
         switch self {
         case .value(let element):
-            hasher.combine(element.objectID)
+            hasher.combine(element)
         case .empty(let section, let row):
             hasher.combine(section)
             hasher.combine(row)
